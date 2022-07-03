4d8eabf1f85169d44d8feaf55eb168cb23226f18
aidanaden <aidanaden@hotmail.com>
2022-07-04 05:13:41 +0800

    chore: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,25 @@
 import withSolid from "rollup-preset-solid";
 
-export default withSolid();
+export default withSolid({
+  input: "src/index.tsx",
+  plugins: [typescript()],
+  external: ["highlight.js/lib/core", "solid"],
+  output: [
+    {
+      file: "dist/highlightjs-solid.min.js",
+      format: "iife",
+      name: "hljsSolidPlugin",
+      interop: "default",
+      globals: {
+        "highlight.js/lib/core": "hljs",
+        solid: "Solid",
+      },
+    },
+    {
+      file: "dist/highlightjs-solid.esm.min.js",
+      format: "es",
+      interop: "default",
+      exports: "default",
+    },
+  ],
+});
